f375e7ebd5b06edb43be01cfdbda1080fbbcf6b1
monali221 <monalisa.banerjee8991@gmail.com>
2021-01-09 15:16:06 +0530

    First commit of python code

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+# A DICTIONARY WITH LIMITED WORDS
+
+d1={"aluminium":"a light metal",
+    "amplifier":"electrical mechine that makes sounds louder",
+    "booklet":"a small thin book",
+    "bouquet":"a bunch of flowers that u give or get as a present"}
+
+# keyvalue=input("Enter search:")
+# print(d1[keyvalue])
+
+print("Enter any of the following keywords to know more:")
+print("aluminium")
+print("amplifier")
+print("booklet")
+print("bouquet")
+
+keyvalue=input()
+print(keyvalue,"=",d1[keyvalue])
